7ce85493b2a1afe738458d4abc9dee703c4db7af
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-26 17:37:51 -0300

    feat: add css variables to global styles

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,5 +1,10 @@
 import { JSX } from 'react';
 
 export default function Home(): JSX.Element {
-  return <h1>My Trips</h1>;
+  return (
+    <div>
+      <h1>My Trips</h1>
+      <a href="#">Link</a>
+    </div>
+  );
 }
