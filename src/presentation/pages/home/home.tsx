c4f60c7c9cb2a7e76c9d912fee136c01a3daacdf
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-28 10:56:59 -0300

    refactor: set map to have no default markers

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -5,17 +5,5 @@ const Map = dynamic(() => import('@/presentation/components/map/map'), {
 });
 
 export default function Home() {
-  const places = [
-    {
-      id: '1',
-      name: 'Valinhos',
-      slug: 'valinhos',
-      location: {
-        latitude: -22.9697,
-        longitude: -46.9974
-      }
-    }
-  ];
-
-  return <Map places={places} />;
+  return <Map />;
 }
