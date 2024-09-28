56dc00ffce527b6c4a373d1f098d79ae438e1f64
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-28 10:48:44 -0300

    test: ensure map will display places with the marker in the correct position

--- src/presentation/components/map/map.spec.tsx
+++ src/presentation/components/map/map.spec.tsx
@@ -1,5 +1,6 @@
 import Map from './map';
 import { render, screen } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
 
 describe('Map Component', () => {
   test('should render without any marker', () => {
@@ -10,5 +11,22 @@ describe('Map Component', () => {
         name: /openstreetmap/i
       })
     ).toBeInTheDocument();
+  });
+
+  test('should render with the marker in the correct position', () => {
+    const places = Array.from({ length: 2 }, () => ({
+      id: faker.string.uuid(),
+      name: faker.location.city(),
+      slug: faker.location.city().toLowerCase(),
+      location: {
+        latitude: faker.location.latitude(),
+        longitude: faker.location.longitude()
+      }
+    }));
+    render(<Map places={places} />);
+
+    places.forEach((place) =>
+      expect(screen.getByTitle(new RegExp(place.slug, 'i'))).toBeInTheDocument()
+    );
   });
 });
