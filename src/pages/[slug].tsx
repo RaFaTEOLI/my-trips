4124c119de7e2ffff73ebe544ec44d7790c55ee2
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-30 18:55:08 -0300

    feat: add slug page

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,55 @@
+import client from '@/graphql/client';
+import { GET_PAGE_BY_SLUG, GET_PAGES } from '@/graphql/queries';
+import { PageTemplate } from '@/presentation/pages';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { useRouter } from 'next/dist/client/router';
+
+type Page = {
+  id: string;
+  heading: string;
+  slug: string;
+  body: {
+    html: string;
+  };
+};
+
+export default function Page({
+  heading,
+  body
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
+  return <PageTemplate heading={heading} body={body} />;
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request<{ pages: Page[] }>(GET_PAGES, {
+    first: 3
+  });
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }));
+
+  return { paths, fallback: true };
+}
+
+export const getStaticProps = (async ({ params }) => {
+  const { page } = await client.request<{ page: Page }>(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  });
+
+  if (!page) return { notFound: true };
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  };
+}) satisfies GetStaticProps<{
+  heading: string;
+  body: string;
+}>;
