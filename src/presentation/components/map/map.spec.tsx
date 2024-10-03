31ee24670d45d1e5b10814b3ee71daa59cc83863
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-10-03 17:29:12 -0300

    test: ensure map will navigate to place page on marker click

--- src/presentation/components/map/map.spec.tsx
+++ src/presentation/components/map/map.spec.tsx
@@ -1,6 +1,8 @@
 import Map from './map';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { faker } from '@faker-js/faker';
+import mockRouter from 'next-router-mock';
 
 describe('Map Component', () => {
   test('should render without any marker', () => {
@@ -26,7 +28,26 @@ describe('Map Component', () => {
     render(<Map places={places} />);
 
     places.forEach((place) =>
-      expect(screen.getByTitle(new RegExp(place.slug, 'i'))).toBeInTheDocument()
+      expect(screen.getByTitle(new RegExp(place.name, 'i'))).toBeInTheDocument()
     );
+  });
+
+  test('should navigate to place page on marker click', async () => {
+    const places = Array.from({ length: 2 }, () => ({
+      id: faker.string.uuid(),
+      name: faker.location.city(),
+      slug: faker.location.city().toLowerCase(),
+      location: {
+        latitude: faker.location.latitude(),
+        longitude: faker.location.longitude()
+      }
+    }));
+    render(<Map places={places} />);
+
+    await userEvent.click(screen.getByTitle(new RegExp(places[0].name, 'i')));
+    expect(mockRouter).toMatchObject({
+      asPath: `/place/${places[0].slug}`,
+      pathname: `/place/${places[0].slug}`
+    });
   });
 });
